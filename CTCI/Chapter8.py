80a9be78ee686f934b260406a24647c0eab6ad3d
MironencoMircea <mircea.mironenco@gmail.com>
2016-07-27 17:11:52 +0300

    added dp solutions

--- CTCI/Chapter8.py
+++ CTCI/Chapter8.py
@@ -43,6 +43,37 @@ class Solution:
         self.paths.pop()
 
 """
+8.3 Magic Index
+"""
+# With distinct elements
+def magic_index(array):
+    start = 0
+    end = len(array) - 1
+    while start >= end:
+        i = (start + end) // 2
+        if array[i] == i:
+            return i
+        elif array[i] > i:
+            end = i - 1
+        else:
+            start = i + 1
+
+    # Haven't found magic index
+    return -1
+
+# # With possible duplicate elements
+# def magic_index_dup(array):
+#     start = 0
+#     end = len(array) - 1
+#     while start >= end:
+#         i = (start + end) // 2
+#         if array[i] == i:
+#             return i
+#         elif array[i] > i:
+
+
+
+"""
 8.4 Power Set
 """
 
@@ -68,6 +99,35 @@ def multiply_recursive(a,b):
     a = a + multiply_recursive(a, b-1)
     return a
 
+"""
+8.6 Towers of Hanoi
+"""
+def move_disks(n, origin, destination ,buffer):
+    if n == 0:
+        return
+    move_disks(n-1,origin,buffer,destination)
+    move_top(origin,destination)
+    move_disks(n-1,buffer,destination,origin)
+
+
+def move_top(origin,destination):
+    v = origin.pop()
+    destination.append(v)
+
+"""
+8.7 & 8.8 Permutations
+"""
+def perm(input):
+    # iterative solution
+    permutations = [[]]
+    for n in input:
+        new_perms = []
+        for perm in permutations:
+            for i in range(len(perm)+1):
+                new_perms.append(perm[:i]+[n]+perm[i:])
+                if i < len(perm) and perm[i] == n: break
+        permutations = new_perms
+    return permutations
 
 """
 8.9 Parens
@@ -75,22 +135,28 @@ def multiply_recursive(a,b):
 
 
 def compute(n):
-    string_array = ['.'] * (2*n)
+    string_array = []
     list = []
-    compute_parens(list,n,n,string_array,0)
+    compute_parens(list,n,n,string_array)
+    for i,l in enumerate(list):
+        l = ''.join(e for e in l)
+        list[i] = l
     return list
 
-def compute_parens(list,left,right,current,count):
+
+def compute_parens(list,left,right,current):
     if right < 0 or left > right:
         return
 
     if left == 0 and right == 0:
-        list.append(current)
+        list.append(current[:])
     else:
         if left > 0:
-            current[count] = '('
-            compute_parens(list,left-1,right,current,count+1)
+            current.append('(')
+            compute_parens(list,left-1,right,current)
+            current.pop()
 
         if right > left:
-            current[count] = ')'
-            compute_parens(list,left,right-1,current,count+1)
+            current.append(')')
+            compute_parens(list,left,right-1,current)
+            current.pop()
